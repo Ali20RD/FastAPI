aaca199f16375e83b024b05b972bc429308ac123
AliRD <ali.rahmanid@yahoo.com>
2026-06-11 00:26:04 -0700

    db connection have been edited.

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://book_user:book_password@localhost:5433/store_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://book_user:book_password@localhost:5432/store_db"
 
 
 
